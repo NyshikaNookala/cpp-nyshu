89c07bd09305de00cb37f3cfa44b45a26bd8ec4a
NyshikaNookala <95057630+NyshikaNookala@users.noreply.github.com>
2022-03-12 11:20:03 +0530

    Create while loop.cpp

---
+++ while loop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cin>>i;
+    while(true)
+    {
+        cout<<i;//0 1 2 3 4 5 6 7 8 9
+        i++;
+        if(i==10)break;//10=10 true
+    }
+}
+//
